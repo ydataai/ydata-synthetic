e77fc31d1008bf31c89b0707c6dcdcb657c41393
fabiana.clemente <fabiana.clemente@ydata.ai>
2020-05-04 15:51:15 +0200

    feat(gan): Add save and load generator from a trained synthesizer

--- models/gan/model.py
+++ models/gan/model.py
@@ -5,6 +5,7 @@ import pandas as pd
 import tensorflow as tf
 from tensorflow.keras.layers import Input, Dense, Dropout
 from tensorflow.keras import Model
+from tensorflow.keras.models import save_model, load_model
 
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import plot_model
@@ -60,7 +61,7 @@ class GAN():
         return np.reshape(x, (batch_size, -1))
         
     def train(self, data, train_arguments):
-        [cache_prefix, epochs, sample_interval, data_dir] = train_arguments
+        [cache_prefix, epochs, sample_interval] = train_arguments
         
         data_cols = data.columns
 
@@ -97,7 +98,7 @@ class GAN():
             if epoch % sample_interval == 0:
                 #Test here data generation step
                 # save model checkpoints
-                model_checkpoint_base_name = data_dir + cache_prefix + '_{}_model_weights_step_{}.h5'
+                model_checkpoint_base_name = './cache/' + cache_prefix + '_{}_model_weights_step_{}.h5'
                 self.generator.save_weights(model_checkpoint_base_name.format('generator', epoch))
                 self.discriminator.save_weights(model_checkpoint_base_name.format('discriminator', epoch))
 
@@ -110,11 +111,19 @@ class GAN():
         assert os.path.isdir(path)==True, \
             "Please provide a valid path. Path must be a directory."
         model_path = os.path.join(path, name)
-        self.generator.save_weights(model_path) #Load the generator
-        return 
+
+        save_model(
+            self.generator, model_path, overwrite=True, include_optimizer=True, save_format=None,
+            signatures=None, options=None
+        )
     
-    def load(self):
-        return
+    def load(self, path):
+        assert os.path.isdir(path) == True, \
+            "Please provide a valid path. Path must be a directory."
+        self.generator = Generator(self.batch_size)
+        if __name__ == '__main__':
+            self.generator = self.generator.load_weights(path)
+        return self.generator
     
 class Generator():
     def __init__(self, batch_size):
@@ -141,18 +150,7 @@ class Discriminator():
         x = Dense(dim, activation='relu')(x)
         x = Dense(1, activation='sigmoid')(x)
         return Model(inputs=input, outputs=x)
-    
-if __name__ == '__main__':
-    
-    data = pd.read_csv('../../data/data_processed.csv', index_col=[0])
-    filt_data = data[data['Class'] == 1]
-    gan_args = [128, 0.00002, 32, filt_data.shape[1], 128]
-    train_args = ['', 300, 50, '.']
-    
-    GAN_synth = GAN(gan_args)
-    GAN_synth.train(filt_data, train_args)
-    
-    
+
     
 
 
