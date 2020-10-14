eb2c1fbb65df0af26413a3b88d9df92a19add9eb
Fabiana <fabiana.clemente@ydata.ai>
2020-10-15 00:39:46 +0100

    fix: Init file.

--- src/ydata_synthetic/synthesizers/__init__.py
+++ src/ydata_synthetic/synthesizers/__init__.py
@@ -1,6 +1,6 @@
-from src.ydata_synthetic.synthesizers.regular.cgan.model import CGAN
-from src.ydata_synthetic.synthesizers.regular.wgan.model import WGAN
-from src.ydata_synthetic.synthesizers.regular.vanillagan.model import VanilllaGAN
+from ydata_synthetic.synthesizers.regular.cgan.model import CGAN
+from ydata_synthetic.synthesizers.regular.wgan.model import WGAN
+from ydata_synthetic.synthesizers.regular.vanillagan.model import VanilllaGAN
 
 __all__ = [
     "VanilllaGAN",
