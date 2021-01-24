b3d6df91ab34f3126fcab7112281bf3a38046325
fabclmnt <fabiana.clemente@ydata.ai>
2021-01-24 15:37:22 +0000

    feat(timeseries): Add time-series stock example.

---
+++ src/ydata_synthetic/preprocessing/timeseries/stock.py
@@ -0,0 +1,28 @@
+"""
+    Get the stock data from Yahoo finance data
+    Data from the period 01 January 2017 - 24 January 2021
+"""
+import os
+import requests as req
+import pandas as pd
+
+from ydata_synthetic.preprocessing.timeseries.utils import real_data_loading
+
+def transformations(seq_len: int):
+    try:
+        stock_df = pd.read_csv('../data/stock.csv')
+    except:
+        stock_url = 'https://query1.finance.yahoo.com/v7/finance/download/GOOG?period1=1483228800&period2=1611446400&interval=1d&events=history&includeAdjustedClose=true'
+        request = req.get(stock_url)
+        url_content = request.content
+
+        file_path = os.path.join(os.path.dirname(os.path.join('..', os.path.dirname(__file__))), 'data')
+        stock_csv = open(os.path.join(file_path, 'stock.csv'), 'wb')
+        stock_csv.write(url_content)
+        # Reading the stock data
+        stock_df = pd.read_csv('../data/stock.csv')
+
+    #Data transformations to be applied prior to be used with the synthesizer model
+    processed_data = real_data_loading(stock_df, seq_len=seq_len)
+
+    return processed_data
