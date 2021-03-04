30420a0d8b8b1b128fa5e3e921191a94f6999f9d
fabclmnt <fabiana.clemente@ydata.ai>
2021-03-04 10:57:55 +0000

    feat(ragular): DRAGAN example.

--- examples/regular/adult_dragan.py
+++ examples/regular/adult_dragan.py
@@ -7,13 +7,13 @@ data, processed_data, preprocessor = transformations()
 # WGAN_GP training
 #Defininf the training parameters of WGAN_GP
 
-noise_dim = 32
+noise_dim = 128
 dim = 128
-batch_size = 200
+batch_size = 500
 
 log_step = 100
-epochs = 1000
-learning_rate = 5e-4
+epochs = 200+1
+learning_rate = 1e-5
 beta_1 = 0.5
 beta_2 = 0.9
 models_dir = './cache'
@@ -21,7 +21,7 @@ models_dir = './cache'
 gan_args = [batch_size, learning_rate, beta_1, beta_2, noise_dim, processed_data.shape[1], dim]
 train_args = ['', epochs, log_step]
 
-synthesizer = DRAGAN(gan_args, n_discriminator=1)
+synthesizer = DRAGAN(gan_args, n_discriminator=3)
 synthesizer.train(processed_data, train_args)
 
 synth_data = synthesizer.sample(1000)
