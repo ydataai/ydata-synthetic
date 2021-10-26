8d18f3b4c1b7e7f776e1f726c7aa11e46f4b00f3
Francisco Santos <francisco.santos@ydata.ai>
2021-10-26 11:58:41 +0100

    standardize transformations method, fix example

--- src/ydata_synthetic/preprocessing/regular/credit_fraud.py
+++ src/ydata_synthetic/preprocessing/regular/credit_fraud.py
@@ -1,4 +1,4 @@
-#Data transformations to be aplied 
+#Data transformations to be aplied
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,9 +8,11 @@ from sklearn.preprocessing import PowerTransformer
 
 def transformations(data):
     #Log transformation to Amount variable
+    processed_data = data.copy()
     data_cols = list(data.columns[data.columns != 'Class'])
-  
+
     #data[data_cols] = StandardScaler().fit_transform(data[data_cols])
-    data[data_cols] = PowerTransformer(method='yeo-johnson', standardize=True, copy=True).fit_transform(data[data_cols])
-    
-    return data
+    transformer = PowerTransformer(method='yeo-johnson', standardize=True, copy=True)
+    processed_data[data_cols] = transformer.fit_transform(data[data_cols])
+
+    return data, processed_data, transformer
