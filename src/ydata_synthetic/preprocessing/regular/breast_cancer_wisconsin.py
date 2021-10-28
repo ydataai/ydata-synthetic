3d8c2a1fe14f2f90dcce77717b0884c0d3173dc7
Fabiana <30911746+fabclmnt@users.noreply.github.com>
2021-10-28 10:49:27 +0100

    Revert "feat(processing): Add inverse preprocess transformations (#91)"
    This reverts commit 963a74f5e6dc4450fe2fec74a0da0739ca6c846c.

--- src/ydata_synthetic/preprocessing/regular/breast_cancer_wisconsin.py
+++ src/ydata_synthetic/preprocessing/regular/breast_cancer_wisconsin.py
@@ -6,18 +6,22 @@ from sklearn.compose import ColumnTransformer
 
 from pmlb import fetch_data
 
-def transformations():
-    data = fetch_data('breast_cancer_wisconsin')
-
+def transformations(auto=True):
+    if auto:
+        data = fetch_data('breast_cancer_wisconsin')
+    else:
+        data = fetch_data('breast_cancer_wisconsin')
+        
     scaler = StandardScaler()
     processed_data = scaler.fit_transform(data)
     processed_data = pd.DataFrame(processed_data)
-
+    
     return data, processed_data, scaler
 
 
 if __name__ == '__main__':
-
-    data = transformations()
-
+    
+    data = transformations(auto=True)
+    
     print(data)
+    
--- src/ydata_synthetic/preprocessing/regular/credit_fraud.py
+++ src/ydata_synthetic/preprocessing/regular/credit_fraud.py
@@ -1,23 +1,16 @@
-#Data transformations to be applied
+#Data transformations to be aplied 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import math
 
 from sklearn.preprocessing import PowerTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
 
 def transformations(data):
     #Log transformation to Amount variable
-    processed_data = data.copy()
     data_cols = list(data.columns[data.columns != 'Class'])
-
-    data_transformer = Pipeline(steps=[
-        ('PowerTransformer', PowerTransformer(method='yeo-johnson', standardize=True, copy=True))])
-
-    preprocessor = ColumnTransformer(
-        transformers = [('power', data_transformer, data_cols)])
-    processed_data[data_cols] = preprocessor.fit_transform(data[data_cols])
-
-    return data, processed_data, preprocessor
+  
+    #data[data_cols] = StandardScaler().fit_transform(data[data_cols])
+    data[data_cols] = PowerTransformer(method='yeo-johnson', standardize=True, copy=True).fit_transform(data[data_cols])
+    
+    return data
