c618d90b9037baf2973a95b2483f1fa131664bff
Fabiana <30911746+fabclmnt@users.noreply.github.com>
2021-10-28 11:56:09 +0100

    feat: Add inverse transformations to supported datasets (#104)

--- src/ydata_synthetic/preprocessing/regular/adult.py
+++ src/ydata_synthetic/preprocessing/regular/adult.py
@@ -9,26 +9,28 @@ from pmlb import fetch_data
 def transformations():
     data = fetch_data('adult')
 
-    numerical_features = ['age', 'fnlwgt', 
+    numerical_features = ['age', 'fnlwgt',
                           'capital-gain', 'capital-loss',
                           'hours-per-week']
     numerical_transformer = Pipeline(steps=[
-        ('onehot', StandardScaler())])
+        ('scaler', StandardScaler())])
 
-    categorical_features = ['workclass','education', 'marital-status', 
+    categorical_features = ['workclass','education', 'marital-status',
                             'occupation', 'relationship',
                             'race', 'sex']
     categorical_transformer = Pipeline(steps=[
         ('onehot', OneHotEncoder(handle_unknown='ignore'))])
 
+    remaining_features = ['education-num', 'native-country','target']
+    remaining_transformer = 'passthrough'
     preprocessor = ColumnTransformer(
         transformers=[
             ('num', numerical_transformer, numerical_features),
-            ('cat', categorical_transformer, categorical_features)])
+            ('cat', categorical_transformer, categorical_features),
+            ('remaining', remaining_transformer, remaining_features)])
 
     processed_data = pd.DataFrame.sparse.from_spmatrix(preprocessor.fit_transform(data))
 
     return data, processed_data, preprocessor
 
 
-    
--- src/ydata_synthetic/preprocessing/regular/credit_fraud.py
+++ src/ydata_synthetic/preprocessing/regular/credit_fraud.py
@@ -1,16 +1,23 @@
-#Data transformations to be aplied 
+#Data transformations to be applied
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import math
 
 from sklearn.preprocessing import PowerTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
 
 def transformations(data):
     #Log transformation to Amount variable
+    processed_data = data.copy()
     data_cols = list(data.columns[data.columns != 'Class'])
-  
-    #data[data_cols] = StandardScaler().fit_transform(data[data_cols])
-    data[data_cols] = PowerTransformer(method='yeo-johnson', standardize=True, copy=True).fit_transform(data[data_cols])
-    
-    return data
+
+    data_transformer = Pipeline(steps=[
+        ('PowerTransformer', PowerTransformer(method='yeo-johnson', standardize=True, copy=True))])
+
+    preprocessor = ColumnTransformer(
+        transformers = [('power', data_transformer, data_cols)])
+    processed_data[data_cols] = preprocessor.fit_transform(data[data_cols])
+
+    return data, processed_data, preprocessor
