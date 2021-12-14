5b05c342441faf9000661f996a061a82b9ab2165
Francisco Santos <francisco.santos@ydata.ai>
2021-12-14 16:38:53 +0000

    Remove test snippet

--- src/ydata_synthetic/synthesizers/regular/cgan/model.py
+++ src/ydata_synthetic/synthesizers/regular/cgan/model.py
@@ -232,22 +232,3 @@ class Discriminator():
         x = Dense(dim, activation='relu')(x)
         x = Dense(1, activation='sigmoid')(x)
         return Model(inputs=[events, label], outputs=x)
-
-import tensorflow as tf
-import numpy as np
-
-model = tf.keras.Sequential()
-model.add(tf.keras.layers.Embedding(10, 64, input_length=10, dtype=str))
-# The model will take as input an integer matrix of size (batch,
-# input_length), and the largest integer (i.e. word index) in the input
-# should be no larger than 999 (vocabulary size).
-# Now model.output_shape is (None, 10, 64), where `None` is the batch
-# dimension.
-test_phrase = "Eu gosto é de comer laranjas e bananas com nutella".split(' ')
-input_array = np.random.randint(10, size=(32, 10))
-to_text = lambda x: test_phrase[x]
-vfunc = np.vectorize(to_text)
-input_array = vfunc(input_array)
-model.compile('rmsprop', 'mse')
-output_array = model.predict(input_array)
-print(output_array.shape)
