bb1e22dce343845aebb222b602599887216793b4
Claudio <fanconic@gmail.com>
2022-08-20 22:07:12 +0200

    changed "train" to "fit" in the super call
    Because of the name change from of the fitting method from "train" to "fit", there are some inconsistencies. Changed the super call, is it would break there

--- src/ydata_synthetic/synthesizers/regular/wgangp/model.py
+++ src/ydata_synthetic/synthesizers/regular/wgangp/model.py
@@ -142,7 +142,7 @@ class WGAN_GP(BaseModel):
             num_cols: List of columns of the data object to be handled as numerical
             cat_cols: List of columns of the data object to be handled as categorical
         """
-        super().train(data, num_cols, cat_cols)
+        super().fit(data, num_cols, cat_cols)
 
         processed_data = self.processor.transform(data)
         self.data_dim = processed_data.shape[1]
