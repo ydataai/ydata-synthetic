77c247ef58961313d97ee67713f10337535e363b
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:51:32 +0600

    improve

--- src/ydata_synthetic/synthesizers/regular/ctgan/model.py
+++ src/ydata_synthetic/synthesizers/regular/ctgan/model.py
@@ -1,10 +1,6 @@
 from functools import partial
 from joblib import dump
 import numpy as np
-from pandas import DataFrame
-import tensorflow as tf
-from keras.layers import \
-    (Input, Dense, LeakyReLU, Dropout, BatchNormalization, ReLU, Concatenate)
-from keras import Model
+import pandas as pd
 
 
