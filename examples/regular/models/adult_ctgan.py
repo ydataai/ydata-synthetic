bc49153fe33b1663ec0ab2f474408b084a1951c2
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:52:20 +0600

    improve

--- examples/regular/models/adult_ctgan.py
+++ examples/regular/models/adult_ctgan.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from pmlb import fetch_data
 
 from ydata_synthetic.synthesizers.regular import RegularSynthesizer
@@ -11,24 +12,31 @@ cat_cols = ['workclass','education', 'education-num', 'marital-status', 'occupat
 
 # Defining the training parameters
 batch_size = 500
-epochs = 500+1
+epochs = 500 + 1
 learning_rate = 2e-4
 beta_1 = 0.5
 beta_2 = 0.9
 
+# Define the model parameters
 ctgan_args = ModelParameters(batch_size=batch_size,
-                             lr=learning_rate,
+                             learning_rate=learning_rate,
                              betas=(beta_1, beta_2))
 
+# Define the training arguments
 train_args = TrainParameters(epochs=epochs)
+
+# Initialize the synthesizer
 synth = RegularSynthesizer(modelname='ctgan', model_parameters=ctgan_args)
+
+# Train the synthesizer
 synth.fit(data=data, train_arguments=train_args, num_cols=num_cols, cat_cols=cat_cols)
 
+# Save the trained synthesizer
 synth.save('adult_ctgan_model.pkl')
 
-#########################################################
-#    Loading and sampling from a trained synthesizer    #
-#########################################################
+# Load the trained synthesizer
 synth = RegularSynthesizer.load('adult_ctgan_model.pkl')
+
+# Sample data from the trained synthesizer
 synth_data = synth.sample(1000)
 print(synth_data)
