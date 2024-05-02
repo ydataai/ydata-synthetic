a009ff234417b8d5f7f3813d5443dc6a9b20c5b2
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:51:12 +0600

    improve

--- src/ydata_synthetic/preprocessing/timeseries/utils.py
+++ src/ydata_synthetic/preprocessing/timeseries/utils.py
@@ -1,15 +1,13 @@
 """
-    Utility functions to be shared by the time-series preprocessing required to feed the data into the synthesizers
+Utility functions to be shared by the time-series preprocessing required to feed the data into the synthesizers
 """
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 
-# Method implemented here: https://github.com/jsyoon0823/TimeGAN/blob/master/data_loading.py
-# Originally used in TimeGAN research
-def real_data_loading(data: np.array, seq_len):
+def real_data_loading(data: np.ndarray, seq_len: int):
     """Load and preprocess real-world datasets.
     Args:
-      - data_name: Numpy array with the values from a a Dataset
+      - data: Numpy array with the values from a dataset
       - seq_len: sequence length
 
     Returns:
@@ -24,13 +22,11 @@ def real_data_loading(data: np.array, seq_len):
     # Preprocess the dataset
     temp_data = []
     # Cut data by sequence length
-    for i in range(0, len(ori_data) - seq_len):
+    for i in range(0, len(ori_data) - seq_len + 1):
         _x = ori_data[i:i + seq_len]
         temp_data.append(_x)
 
-    # Mix the datasets (to make it similar to i.i.d)
+    # Shuffle the datasets (to make it similar to i.i.d)
     idx = np.random.permutation(len(temp_data))
-    data = []
-    for i in range(len(temp_data)):
-        data.append(temp_data[idx[i]])
+    data = temp_data[idx]
     return data
