d9f8fbe78f7417795342e38e83a1293b095b68c0
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:52:18 +0600

    improve

--- src/ydata_synthetic/synthesizers/loss.py
+++ src/ydata_synthetic/synthesizers/loss.py
@@ -1,8 +1,6 @@
-from tensorflow import \
-    (random, reshape, shape, GradientTape, reduce_mean, 
-     norm as tfnorm, tile, constant, int32)
-from tensorflow.math import reduce_std, reduce_euclidean_norm
-from enum import Enum
+import tensorflow as tf
+from tensorflow.keras import Input
+from tensorflow.keras.models import Model
 
 class Mode(Enum):
     WGANGP = 'wgangp'
@@ -10,52 +8,65 @@ class Mode(Enum):
     CRAMER = 'cramer'
     CTGAN = 'ctgan'
 
-## Original code loss from
-## https://github.com/LynnHo/DCGAN-LSGAN-WGAN-GP-DRAGAN-Tensorflow-2/blob/master/tf2gan/loss.py
-def gradient_penalty(f, real, fake, mode, pac=None):
-    def _gradient_penalty(f, real, fake=None):
-        def _interpolate(a, b=None):
-            if b is None:   # interpolation in DRAGAN
-                beta = random.uniform(shape=shape(a), minval=0., maxval=1.)
-                b = a + 0.5 * reduce_std(a) * beta
-            shape_ = [shape(a)[0]] + [1] * (a.shape.ndims - 1)
-            alpha = random.uniform(shape=shape_, minval=0., maxval=1.)
-            inter = a + alpha * (b - a)
-            inter.set_shape(a.shape)
-            return inter
+def gradient_penalty(f, real, fake, mode: Mode, pac=None):
+    """
+    Compute the gradient penalty for a given discriminator.
 
+    Args:
+        f: A function that takes a tensor as input and outputs a tensor.
+        real: A tensor representing real data.
+        fake: A tensor representing fake data.
+        mode: The mode of gradient penalty to compute.
+        pac: An integer specifying the number of partitions for CTGAN.
+
+    Returns:
+        A tensor representing the gradient penalty.
+    """
+    def _interpolate(a, b=None):
+        if b is None:
+            beta = tf.random.uniform(shape=tf.shape(a), minval=0., maxval=1.)
+            b = a + 0.5 * tf.math.reduce_std(a) * beta
+        shape_ = [tf.shape(a)[0]] + [1] * (a.shape.ndims - 1)
+        alpha = tf.random.uniform(shape=shape_, minval=0., maxval=1.)
+        inter = a + alpha * (b - a)
+        inter.set_shape(a.shape)
+        return inter
+
+    def _gradient_penalty(f, real, fake=None):
         x = _interpolate(real, fake)
-        with GradientTape() as t:
+        with tf.GradientTape() as t:
             t.watch(x)
             pred = f(x)
         grad = t.gradient(pred, x)
-        norm = tfnorm(reshape(grad, [shape(grad)[0], -1]), axis=1)
-        gp = reduce_mean((norm - 1.)**2)
+        norm = tf.norm(tf.reshape(grad, [tf.shape(grad)[0], -1]), axis=1)
+        gp = tf.reduce_mean((norm - 1.)**2)
         return gp
 
     def _gradient_penalty_cramer(f_crit, real, fake):
-        epsilon = random.uniform([real.shape[0], 1], 0.0, 1.0)
+        epsilon = tf.random.uniform([real.shape[0], 1], 0.0, 1.0)
         x_hat = epsilon * real + (1 - epsilon) * fake[0]
-        with GradientTape() as t:
+        with tf.GradientTape() as t:
             t.watch(x_hat)
             f_x_hat = f_crit(x_hat, fake[1])
         gradients = t.gradient(f_x_hat, x_hat)
-        c_dx = tfnorm(reshape(gradients, [shape(gradients)[0], -1]), axis=1)
+        c_dx = tf.norm(tf.reshape(gradients, [tf.shape(gradients)[0], -1]), axis=1)
         c_regularizer = (c_dx - 1.0) ** 2
         return c_regularizer
 
     def _gradient_penalty_ctgan(f, real, fake, pac=10):
-        alpha = random.uniform([real.shape[0] // pac, 1, 1], 0., 1.)
-        alpha = tile(alpha, constant([1, pac, real.shape[1]], int32))
-        alpha = reshape(alpha, [-1, real.shape[1]])
+        if pac is None:
+            raise ValueError("For CTGAN mode, pac argument must be provided.")
+        alpha = tf.random.uniform([real.shape[0] // pac, 1, 1], 0., 1.)
+        alpha = tf.tile(alpha, constant([1, pac, real.shape[1]], tf.int32))
+        alpha = tf.reshape(alpha, [-1, real.shape[1]])
         interpolate = alpha * real + ((1 - alpha) * fake)
-        with GradientTape() as tape:
+        with tf.GradientTape() as tape:
             tape.watch(interpolate)
             prediction = f(interpolate)
         gradient = tape.gradient(prediction, [interpolate])[0]
-        gradient = reshape(gradient, constant([-1, pac * real.shape[1]], int32))
-        slope = reduce_euclidean_norm(gradient, axis=1)
-        return reduce_mean((slope - 1.) ** 2)
+        gradient = tf.reshape(gradient, constant([-1, pac * real.shape[1]], tf.int32))
+        slope = tf.reduce_euclidean_norm(gradient, axis=1)
+        return tf.reduce_mean((slope - 1.) ** 2)
 
     if mode == Mode.DRAGAN:
         gp = _gradient_penalty(f, real)
@@ -64,9 +75,22 @@ def gradient_penalty(f, real, fake, mode, pac=None):
     elif mode == Mode.WGANGP:
         gp = _gradient_penalty(f, real, fake)
     elif mode == Mode.CTGAN:
-        if pac is not None:
-            gp = _gradient_penalty_ctgan(f, real, fake, pac=pac)
-        else:
-            gp = _gradient_penalty_ctgan(f, real, fake)
+        gp = _gradient_penalty_ctgan(f, real, fake, pac=pac)
 
     return gp
+
+# Example usage
+# Define a discriminator model
+input_layer = Input(shape=(28, 28, 1))
+x = tf.keras.layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same')(input_layer)
+x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
+x = tf.keras.layers.Dropout(0.3)(x)
+x = tf.keras.layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same')(x)
+x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
+x = tf.keras.layers.Dropout(0.3)(x)
+x = tf.keras.layers.Flatten()(x)
+output_layer = tf.keras.layers.Dense(1, activation='sigmoid')(x)
+discriminator = Model(input_layer, output_layer)
+
+# Compute the gradient penalty
+gp = gradient_penalty(discriminator, real_images, fake_images, mode=Mode.WGANGP)
