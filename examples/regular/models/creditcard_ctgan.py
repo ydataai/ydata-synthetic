d1b87aba498d65fcf0cba958f321ae3b3627f35b
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:51:06 +0600

    comment

--- examples/regular/models/creditcard_ctgan.py
+++ examples/regular/models/creditcard_ctgan.py
@@ -1,62 +1,62 @@
 """
     CTGAN architecture example file
+
+This script demonstrates how to use the CTGAN architecture for synthetic data generation.
+It uses the creditcard dataset available at https://datahub.io/machine-learning/creditcard/r/creditcard.csv
 """
-import pandas as pd
-from sklearn import cluster
 
-from ydata_synthetic.utils.cache import cache_file
-from ydata_synthetic.synthesizers import ModelParameters, TrainParameters
-from ydata_synthetic.synthesizers.regular import RegularSynthesizer
+import pandas as pd  # Importing pandas library for data manipulation
+from sklearn import cluster  # Importing clustering algorithms from sklearn
 
-# Read the original data and have it preprocessed
+# Reading the original data and preprocessing it
 data_path = cache_file('creditcard.csv', 'https://datahub.io/machine-learning/creditcard/r/creditcard.csv')
 data = pd.read_csv(data_path, index_col=[0])
 
 # Data processing and analysis
-num_cols = list(data.columns[ data.columns != 'Class' ])
-cat_cols = []
+num_cols = list(data.columns[data.columns != 'Class'])  # List of numerical columns
+cat_cols = []  # List of categorical columns
 
 print('Dataset columns: {}'.format(num_cols))
 sorted_cols = ['V14', 'V4', 'V10', 'V17', 'V12', 'V26', 'Amount', 'V21', 'V8', 'V11', 'V7', 'V28', 'V19',
-                'V3', 'V22', 'V6', 'V20', 'V27', 'V16', 'V13', 'V25', 'V24', 'V18', 'V2', 'V1', 'V5', 'V15',
-                'V9', 'V23', 'Class']
-processed_data = data[ sorted_cols ].copy()
+               'V3', 'V22', 'V6', 'V20', 'V27', 'V16', 'V13', 'V25', 'V24', 'V18', 'V2', 'V1', 'V5', 'V15',
+               'V9', 'V23', 'Class']
+processed_data = data[sorted_cols].copy()  # Copying the data with sorted columns
 
-# For the purpose of this example we will only synthesize the minority class
+# Selecting the minority class for synthesis
 train_data = processed_data.loc[processed_data['Class'] == 1].copy()
 
-# Create a new class column using KMeans - This will mainly be useful if we want to leverage conditional GAN
+# Creating a new class column using KMeans for conditional GAN
 print("Dataset info: Number of records - {} Number of variables - {}".format(train_data.shape[0], train_data.shape[1]))
-algorithm = cluster.KMeans
-args, kwds = (), {'n_clusters':2, 'random_state':0}
-labels = algorithm(*args, **kwds).fit_predict(train_data[num_cols])
+algorithm = cluster.KMeans  # Using KMeans algorithm
+args, kwds = (), {'n_clusters': 2, 'random_state': 0}  # Initializing the algorithm
+labels = algorithm(*args, **kwds).fit_predict(train_data[num_cols])  # Fitting the algorithm
 
-fraud_w_classes = train_data.copy()
-fraud_w_classes['Class'] = labels
+fraud_w_classes = train_data.copy()  # Copying the data
+fraud_w_classes['Class'] = labels  # Adding the new class column
 
 #----------------------------
 #    CTGAN Training
 #----------------------------
 
-batch_size = 500
-epochs = 500+1
-learning_rate = 2e-4
-beta_1 = 0.5
-beta_2 = 0.9
+batch_size = 500  # Setting batch size
+epochs = 500 + 1  # Setting number of epochs
+learning_rate = 2e-4  # Setting learning rate
+beta_1 = 0.5  # Setting beta1 value
+beta_2 = 0.9  # Setting beta2 value
 
-ctgan_args = ModelParameters(batch_size=batch_size,
+ctgan_args = ModelParameters(batch_size=batch_size,  # Model parameters
                              lr=learning_rate,
                              betas=(beta_1, beta_2))
 
-train_args = TrainParameters(epochs=epochs)
+train_args = TrainParameters(epochs=epochs)  # Training parameters
 
-# Create a bining
-fraud_w_classes['Amount'] = pd.cut(fraud_w_classes['Amount'], 5).cat.codes
+# Preprocessing the data for CTGAN
+fraud_w_classes['Amount'] = pd.cut(fraud_w_classes['Amount'], 5).cat.codes  # Binning the 'Amount' column
 
-# Init the CTGAN
+# Initializing the CTGAN
 synth = RegularSynthesizer(modelname='ctgan', model_parameters=ctgan_args)
 
-#Training the CTGAN
+# Training the CTGAN
 synth.fit(data=fraud_w_classes, train_arguments=train_args, num_cols=num_cols, cat_cols=cat_cols)
 
 # Saving the synthesizer
@@ -66,5 +66,5 @@ synth.save('creditcard_ctgan_model.pkl')
 synthesizer = RegularSynthesizer.load('creditcard_ctgan_model.pkl')
 
 # Sampling from the synthesizer
-sample = synthesizer.sample(1000)
-print(sample)
+sample = synthesizer.sample(1000)  # Sampling 1000 records
+print(sample)  # Printing the sampled records
