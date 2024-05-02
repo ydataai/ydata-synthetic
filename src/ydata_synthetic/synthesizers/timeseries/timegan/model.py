38099d8945b3f49140bbc6534d3731d89f083019
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:52:18 +0600

    improve

--- src/ydata_synthetic/synthesizers/timeseries/timegan/model.py
+++ src/ydata_synthetic/synthesizers/timeseries/timegan/model.py
@@ -2,44 +2,56 @@
     TimeGAN class implemented accordingly with:
     Original code can be found here: https://bitbucket.org/mvdschaar/mlforhealthlabpub/src/master/alg/timegan/
 """
-from tqdm import tqdm
+from typing import Any, Callable, Dict, List, Optional, Tuple
+
+import tensorflow as tf
 import numpy as np
 from pandas import DataFrame
-
-from tensorflow import function, GradientTape, sqrt, abs, reduce_mean, ones_like, zeros_like, convert_to_tensor, float32
+from tensorflow import function
 from tensorflow import data as tfdata
 from tensorflow import nn
-from keras import (Model, Sequential, Input)
-from keras.layers import (GRU, LSTM, Dense)
-from keras.optimizers import Adam
-from keras.losses import (BinaryCrossentropy, MeanSquaredError)
-
+from tensorflow.keras import Model
+from tensorflow.keras.layers import (GRU, LSTM, Dense, Input)
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import (BinaryCrossentropy, MeanSquaredError)
 from ydata_synthetic.synthesizers.base import BaseGANModel, ModelParameters, TrainParameters
 from ydata_synthetic.preprocessing.timeseries.utils import real_data_loading
 
-def make_net(model, n_layers, hidden_units, output_units, net_type='GRU'):
-    if net_type=='GRU':
+def make_net(model: Model,
+             n_layers: int,
+             hidden_units: int,
+             output_units: int,
+             net_type: str = 'GRU') -> Model:
+    if net_type == 'GRU':
         for i in range(n_layers):
             model.add(GRU(units=hidden_units,
-                      return_sequences=True,
-                      name=f'GRU_{i + 1}'))
+                          return_sequences=True,
+                          name=f'GRU_{i + 1}'))
     else:
         for i in range(n_layers):
             model.add(LSTM(units=hidden_units,
-                      return_sequences=True,
-                      name=f'LSTM_{i + 1}'))
+                          return_sequences=True,
+                          name=f'LSTM_{i + 1}'))
 
     model.add(Dense(units=output_units,
                     activation='sigmoid',
                     name='OUT'))
     return model
 
-
 class TimeGAN(BaseGANModel):
-
+    """
+    TimeGAN class.
+    """
     __MODEL__ = 'TimeGAN'
 
-    def __init__(self, model_parameters: ModelParameters):
+    def __init__(self,
+                 model_parameters: ModelParameters):
+        """
+        Initialize the TimeGAN class.
+
+        Args:
+            model_parameters: ModelParameters object.
+        """
         super().__init__(model_parameters)
         self.seq_len = None
         self.n_seq = None
@@ -47,51 +59,75 @@ class TimeGAN(BaseGANModel):
         self.gamma = model_parameters.gamma
         self.num_cols = None
 
-    def fit(self, data: DataFrame,
-        train_arguments: TrainParameters,
-        num_cols: list[str] | None = None,
-        cat_cols: list[str] | None = None):
+    def fit(self,
+            data: DataFrame,
+            train_arguments: TrainParameters,
+            num_cols: List[str],
+            non_seq_cols: Optional[List[str]] = None,
+            cat_cols: Optional[List[str]] = None):
         """
-        Fits the TimeGAN model.
+        Fit the TimeGAN model.
 
         Args:
-            data: A pandas DataFrame with the data to be synthesized.
-            train_arguments: TimeGAN training arguments.
-            num_cols: List of columns to be handled as numerical
-            cat_cols: List of columns to be handled as categorical
+            data: DataFrame object.
+            train_arguments: TrainParameters object.
+            num_cols: List of numerical column names.
+            non_seq_cols: List of non-sequential column names.
+            cat_cols: List of categorical column names.
+
+        Raises:
+            NotImplementedError: If categorical columns are provided.
         """
-        super().fit(data=data, num_cols=num_cols, cat_cols=cat_cols, train_arguments=train_arguments)
+        super().fit(data=data,
+                    num_cols=num_cols,
+                    cat_cols=cat_cols,
+                    train_arguments=train_arguments)
         if cat_cols:
             raise NotImplementedError("TimeGAN does not support categorical features.")
         self.num_cols = num_cols
         self.seq_len = train_arguments.sequence_length
         self.n_seq = train_arguments.number_sequences
-        processed_data = real_data_loading(data[self.num_cols].values, seq_len=self.seq_len)
-        self.train(data=processed_data, train_steps=train_arguments.epochs)
+        processed_data = real_data_loading(data[self.num_cols].values,
+                                           seq_len=self.seq_len)
+        self.train(data=processed_data,
+                   train_steps=train_arguments.epochs)
 
-    def sample(self, n_samples: int):
+    def sample(self,
+               n_samples: int) -> List[DataFrame]:
         """
-        Samples new data from the TimeGAN.
+        Sample new data from the TimeGAN.
 
         Args:
             n_samples: Number of samples to be generated.
+
+        Returns:
+            List of DataFrame objects.
         """
         Z_ = next(self.get_batch_noise(size=n_samples))
         records = self.generator(Z_)
         data = []
         for i in range(records.shape[0]):
-            data.append(DataFrame(records[i], columns=self.num_cols))
+            data.append(DataFrame(records[i],
+                                 columns=self.num_cols))
         return data
 
     def define_gan(self):
-        self.generator_aux=Generator(self.hidden_dim).build()
-        self.supervisor=Supervisor(self.hidden_dim).build()
-        self.discriminator=Discriminator(self.hidden_dim).build()
-        self.recovery = Recovery(self.hidden_dim, self.n_seq).build()
+        """
+        Define the GAN architecture.
+        """
+        self.generator_aux = Generator(self.hidden_dim).build()
+        self.supervisor = Supervisor(self.hidden_dim).build()
+        self.discriminator = Discriminator(self.hidden_dim).build()
+        self.recovery = Recovery(self.hidden_dim,
+                                 self.n_seq).build()
         self.embedder = Embedder(self.hidden_dim).build()
 
-        X = Input(shape=[self.seq_len, self.n_seq], batch_size=self.batch_size, name='RealData')
-        Z = Input(shape=[self.seq_len, self.n_seq], batch_size=self.batch_size, name='RandomNoise')
+        X = Input(shape=[self.seq_len, self.n_seq],
+                  batch_size=self.batch_size,
+                  name='RealData')
+        Z = Input(shape=[self.seq_len, self.n_seq],
+                  batch_size=self.batch_size,
+                  name='RandomNoise')
 
         #--------------------------------
         # Building the AutoEncoder
@@ -99,7 +135,9 @@ class TimeGAN(BaseGANModel):
         H = self.embedder(X)
         X_tilde = self.recovery(H)
 
-        self.autoencoder = Model(inputs=X, outputs=X_tilde)
+        self.autoencoder = Model(inputs=X,
+                                 outputs=X_tilde,
+                                 name='Autoencoder')
 
         #---------------------------------
         # Adversarial Supervise Architecture
@@ -109,8 +147,8 @@ class TimeGAN(BaseGANModel):
         Y_fake = self.discriminator(H_hat)
 
         self.adversarial_supervised = Model(inputs=Z,
-                                       outputs=Y_fake,
-                                       name='AdversarialSupervised')
+                                            outputs=Y_fake,
+                                            name='AdversarialSupervised')
 
         #---------------------------------
         # Adversarial architecture in latent space
@@ -118,15 +156,15 @@ class TimeGAN(BaseGANModel):
         Y_fake_e = self.discriminator(E_Hat)
 
         self.adversarial_embedded = Model(inputs=Z,
-                                    outputs=Y_fake_e,
-                                    name='AdversarialEmbedded')
+                                          outputs=Y_fake_e,
+                                          name='AdversarialEmbedded')
         # ---------------------------------
         # Synthetic data generation
         # ---------------------------------
         X_hat = self.recovery(H_hat)
         self.generator = Model(inputs=Z,
-                            outputs=X_hat,
-                            name='FinalGenerator')
+                               outputs=X_hat,
+                               name='Generator')
 
         # --------------------------------
         # Final discriminator model
@@ -134,246 +172,40 @@ class TimeGAN(BaseGANModel):
         Y_real = self.discriminator(H)
         self.discriminator_model = Model(inputs=X,
                                          outputs=Y_real,
-                                         name="RealDiscriminator")
+                                         name="Discriminator")
 
         # ----------------------------
         # Define the loss functions
         # ----------------------------
-        self._mse=MeanSquaredError()
-        self._bce=BinaryCrossentropy()
-
+        self._mse = MeanSquaredError()
+        self._bce = BinaryCrossentropy()
 
     @function
-    def train_autoencoder(self, x, opt):
+    def train_autoencoder(self,
+                           x: tf.Tensor,
+                           opt: Adam) -> tf.Tensor:
+        """
+        Train the autoencoder.
+
+        Args:
+            x: Input tensor.
+            opt: Adam optimizer.
+
+        Returns:
+            Tensor of the embedding loss.
+        """
         with GradientTape() as tape:
             x_tilde = self.autoencoder(x)
-            embedding_loss_t0 = self._mse(x, x_tilde)
-            e_loss_0 = 10 * sqrt(embedding_loss_t0)
+            embedding_loss_t0 = self._mse(x,
+                                          x_tilde)
+            e_loss_0 = 10 * tf.sqrt(embedding_loss_t0)
 
         var_list = self.embedder.trainable_variables + self.recovery.trainable_variables
-        gradients = tape.gradient(e_loss_0, var_list)
-        opt.apply_gradients(zip(gradients, var_list))
-        return sqrt(embedding_loss_t0)
+        gradients = tape.gradient(e_loss_0,
+                                  var_list)
+        opt.apply_gradients(zip(gradients,
+                                var_list))
+        return tf.sqrt(embedding_loss_t0)
 
     @function
-    def train_supervisor(self, x, opt):
-        with GradientTape() as tape:
-            h = self.embedder(x)
-            h_hat_supervised = self.supervisor(h)
-            generator_loss_supervised = self._mse(h[:, 1:, :], h_hat_supervised[:, :-1, :])
 
-        var_list = self.supervisor.trainable_variables + self.generator.trainable_variables
-        gradients = tape.gradient(generator_loss_supervised, var_list)
-        apply_grads = [(grad, var) for (grad, var) in zip(gradients, var_list) if grad is not None]
-        opt.apply_gradients(apply_grads)
-        return generator_loss_supervised
-
-    @function
-    def train_embedder(self,x, opt):
-        with GradientTape() as tape:
-            # Supervised Loss
-            h = self.embedder(x)
-            h_hat_supervised = self.supervisor(h)
-            generator_loss_supervised = self._mse(h[:, 1:, :], h_hat_supervised[:, :-1, :])
-
-            # Reconstruction Loss
-            x_tilde = self.autoencoder(x)
-            embedding_loss_t0 = self._mse(x, x_tilde)
-            e_loss = 10 * sqrt(embedding_loss_t0) + 0.1 * generator_loss_supervised
-
-        var_list = self.embedder.trainable_variables + self.recovery.trainable_variables
-        gradients = tape.gradient(e_loss, var_list)
-        opt.apply_gradients(zip(gradients, var_list))
-        return sqrt(embedding_loss_t0)
-
-    def discriminator_loss(self, x, z):
-        # Loss on false negatives
-        y_real = self.discriminator_model(x)
-        discriminator_loss_real = self._bce(y_true=ones_like(y_real),
-                                            y_pred=y_real)
-
-        # Loss on false positives
-        y_fake = self.adversarial_supervised(z)
-        discriminator_loss_fake = self._bce(y_true=zeros_like(y_fake),
-                                            y_pred=y_fake)
-
-        y_fake_e = self.adversarial_embedded(z)
-        discriminator_loss_fake_e = self._bce(y_true=zeros_like(y_fake_e),
-                                              y_pred=y_fake_e)
-        return (discriminator_loss_real +
-                discriminator_loss_fake +
-                self.gamma * discriminator_loss_fake_e)
-
-    @staticmethod
-    def calc_generator_moments_loss(y_true, y_pred):
-        y_true_mean, y_true_var = nn.moments(x=y_true, axes=[0])
-        y_pred_mean, y_pred_var = nn.moments(x=y_pred, axes=[0])
-        g_loss_mean = reduce_mean(abs(y_true_mean - y_pred_mean))
-        g_loss_var = reduce_mean(abs(sqrt(y_true_var + 1e-6) - sqrt(y_pred_var + 1e-6)))
-        return g_loss_mean + g_loss_var
-
-    @function
-    def train_generator(self, x, z, opt):
-        with GradientTape() as tape:
-            y_fake = self.adversarial_supervised(z)
-            generator_loss_unsupervised = self._bce(y_true=ones_like(y_fake),
-                                                    y_pred=y_fake)
-
-            y_fake_e = self.adversarial_embedded(z)
-            generator_loss_unsupervised_e = self._bce(y_true=ones_like(y_fake_e),
-                                                      y_pred=y_fake_e)
-            h = self.embedder(x)
-            h_hat_supervised = self.supervisor(h)
-            generator_loss_supervised = self._mse(h[:, 1:, :], h_hat_supervised[:, :-1, :])
-
-            x_hat = self.generator(z)
-            generator_moment_loss = self.calc_generator_moments_loss(x, x_hat)
-
-            generator_loss = (generator_loss_unsupervised +
-                              generator_loss_unsupervised_e +
-                              100 * sqrt(generator_loss_supervised) +
-                              100 * generator_moment_loss)
-
-        var_list = self.generator_aux.trainable_variables + self.supervisor.trainable_variables
-        gradients = tape.gradient(generator_loss, var_list)
-        opt.apply_gradients(zip(gradients, var_list))
-        return generator_loss_unsupervised, generator_loss_supervised, generator_moment_loss
-
-    @function
-    def train_discriminator(self, x, z, opt):
-        with GradientTape() as tape:
-            discriminator_loss = self.discriminator_loss(x, z)
-
-        var_list = self.discriminator.trainable_variables
-        gradients = tape.gradient(discriminator_loss, var_list)
-        opt.apply_gradients(zip(gradients, var_list))
-        return discriminator_loss
-
-    def get_batch_data(self, data, n_windows):
-        data = convert_to_tensor(data, dtype=float32)
-        return iter(tfdata.Dataset.from_tensor_slices(data)
-                                .shuffle(buffer_size=n_windows)
-                                .batch(self.batch_size).repeat())
-
-    def _generate_noise(self):
-        while True:
-            yield np.random.uniform(low=0, high=1, size=(self.seq_len, self.n_seq))
-
-    def get_batch_noise(self, size=None):
-        return iter(tfdata.Dataset.from_generator(self._generate_noise, output_types=float32)
-                                .batch(self.batch_size if size is None else size)
-                                .repeat())
-
-    def train(self, data, train_steps):
-        # Assemble the model
-        self.define_gan()
-
-        ## Embedding network training
-        autoencoder_opt = Adam(learning_rate=self.g_lr)
-        for _ in tqdm(range(train_steps), desc='Emddeding network training'):
-            X_ = next(self.get_batch_data(data, n_windows=len(data)))
-            step_e_loss_t0 = self.train_autoencoder(X_, autoencoder_opt)
-
-        ## Supervised Network training
-        supervisor_opt = Adam(learning_rate=self.g_lr)
-        for _ in tqdm(range(train_steps), desc='Supervised network training'):
-            X_ = next(self.get_batch_data(data, n_windows=len(data)))
-            step_g_loss_s = self.train_supervisor(X_, supervisor_opt)
-
-        ## Joint training
-        generator_opt = Adam(learning_rate=self.g_lr)
-        embedder_opt = Adam(learning_rate=self.g_lr)
-        discriminator_opt = Adam(learning_rate=self.d_lr)
-
-        step_g_loss_u = step_g_loss_s = step_g_loss_v = step_e_loss_t0 = step_d_loss = 0
-        for _ in tqdm(range(train_steps), desc='Joint networks training'):
-
-            #Train the generator (k times as often as the discriminator)
-            # Here k=2
-            for _ in range(2):
-                X_ = next(self.get_batch_data(data, n_windows=len(data)))
-                Z_ = next(self.get_batch_noise())
-                # --------------------------
-                # Train the generator
-                # --------------------------
-                step_g_loss_u, step_g_loss_s, step_g_loss_v = self.train_generator(X_, Z_, generator_opt)
-
-                # --------------------------
-                # Train the embedder
-                # --------------------------
-                step_e_loss_t0 = self.train_embedder(X_, embedder_opt)
-
-            X_ = next(self.get_batch_data(data, n_windows=len(data)))
-            Z_ = next(self.get_batch_noise())
-            step_d_loss = self.discriminator_loss(X_, Z_)
-            if step_d_loss > 0.15:
-                step_d_loss = self.train_discriminator(X_, Z_, discriminator_opt)
-
-
-class Generator(Model):
-    def __init__(self, hidden_dim, net_type='GRU'):
-        self.hidden_dim = hidden_dim
-        self.net_type = net_type
-
-    def build(self):
-        model = Sequential(name='Generator')
-        model = make_net(model,
-                         n_layers=3,
-                         hidden_units=self.hidden_dim,
-                         output_units=self.hidden_dim,
-                         net_type=self.net_type)
-        return model
-
-class Discriminator(Model):
-    def __init__(self, hidden_dim, net_type='GRU'):
-        self.hidden_dim = hidden_dim
-        self.net_type=net_type
-
-    def build(self):
-        model = Sequential(name='Discriminator')
-        model = make_net(model,
-                         n_layers=3,
-                         hidden_units=self.hidden_dim,
-                         output_units=1,
-                         net_type=self.net_type)
-        return model
-
-class Recovery(Model):
-    def __init__(self, hidden_dim, n_seq):
-        self.hidden_dim=hidden_dim
-        self.n_seq=n_seq
-        return
-
-    def build(self):
-        recovery = Sequential(name='Recovery')
-        recovery = make_net(recovery,
-                            n_layers=3,
-                            hidden_units=self.hidden_dim,
-                            output_units=self.n_seq)
-        return recovery
-
-class Embedder(Model):
-
-    def __init__(self, hidden_dim):
-        self.hidden_dim=hidden_dim
-        return
-
-    def build(self):
-        embedder = Sequential(name='Embedder')
-        embedder = make_net(embedder,
-                            n_layers=3,
-                            hidden_units=self.hidden_dim,
-                            output_units=self.hidden_dim)
-        return embedder
-
-class Supervisor(Model):
-    def __init__(self, hidden_dim):
-        self.hidden_dim=hidden_dim
-
-    def build(self):
-        model = Sequential(name='Supervisor')
-        model = make_net(model,
-                         n_layers=2,
-                         hidden_units=self.hidden_dim,
-                         output_units=self.hidden_dim)
-        return model
