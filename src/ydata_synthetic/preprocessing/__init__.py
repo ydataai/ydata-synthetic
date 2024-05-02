200b448f0169d751f72ef769773265660f17c451
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 10:51:05 +0600

    improve

--- src/ydata_synthetic/preprocessing/__init__.py
+++ src/ydata_synthetic/preprocessing/__init__.py
@@ -1,7 +1,9 @@
-from ydata_synthetic.preprocessing.regular.processor import RegularDataProcessor
-from ydata_synthetic.preprocessing.timeseries.timeseries_processor import TimeSeriesDataProcessor
+# Import necessary modules
+from ydata_synthetic.preprocessing.regular import RegularDataProcessor
+from ydata_synthetic.preprocessing.timeseries import TimeSeriesDataProcessor
 
+# Define the list of all available data processors
 __all__ = [
-    "RegularDataProcessor",
-    "TimeSeriesDataProcessor"
+    "RegularDataProcessor",  # Regular data processor
+    "TimeSeriesDataProcessor"  # Time series data processor
 ]
